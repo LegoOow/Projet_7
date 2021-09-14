6729b884b2c164c539b45a693b25ae3ca27c29a3
Kevin <castel.kevin.04@gmail.com>
2021-09-14 16:36:33 +0200

    Begin Front

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <img alt="Groupomania Logo" src="../assets/icon-above-font.png">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/Register">S'inscrire</router-link>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+
+</style>
