6b72865d0a44a2def1840694da86bf585abd21cb
Kevin <castel.kevin.04@gmail.com>
2021-09-30 06:40:27 +0200

    Newpost + wall

--- frontend/src/views/HomeUser.vue
+++ frontend/src/views/HomeUser.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="homeuser">
       <Header />
+      <div class="wall">
+        
+      </div>
   </div>
 </template>
 
@@ -16,5 +19,17 @@ export default {
 </script>
 
 <style>
+
+.wall {
+  display: flex;
+  flex-flow: column;
+  width: 100%;
+  margin-top: 40px;
+  height: 600px;
+  border-radius: 50px;
+  box-shadow: 0px 0px 10px 5px rgba(119, 119, 119, 0.5);
+-moz-box-shadow: 0px 0px 10px 5px rgba(119, 119, 119, 0.5);
+-webkit-box-shadow: 0px 0px 10px 5px rgba(119, 119, 119, 0.5);
+}
 
 </style>
