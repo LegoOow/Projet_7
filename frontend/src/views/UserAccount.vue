178cbb12eb68210fde4a690c20b5652be7977629
Kevin <castel.kevin.04@gmail.com>
2021-10-11 06:30:12 +0200

    Affichage données API

--- frontend/src/views/UserAccount.vue
+++ frontend/src/views/UserAccount.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="useraccount">
       <Header />
-      <h1>Bonjour</h1>
+      <h1>Bonjour {{ user.prenom }}</h1>
     
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 import Header from '../components/Header.vue'
 
@@ -16,10 +17,38 @@ export default {
   components: { 
     Header 
   },
+  
+  data(){
+    return {
+      user:""
+    }
+  },
+  created() {
+    this.displayProfile;
+  },
+  
+  methods: {
+    displayProfile() {
+      const userId = localStorage.getItem('userId');
 
+      axios.get('http://localhost:4000/api/user/' + userId, {
+        headers: {
+						Authorization: 'Bearer ' + localStorage.getItem('token')
+					}
+      })
+      .then(res => {
+        this.user = res.data;
+      })
+      .catch()
+    }
+  }
 }
 
+
+
 </script>
+
+
 
 <style>
 
